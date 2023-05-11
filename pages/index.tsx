3b1337d364ca15f34740180febcc575a251ab7d4
白石拓 <s.taku0718@gmail.com>
2023-05-11 22:13:59 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,57 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+const IndexPage: NextPage = () => {
+// ❶ useStateを使って状態を定義する
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  // ❷ マウント時に画像を読み込む宣言
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url); // 画像URLの状態を更新する
+      setLoading(false); // ローディング状態を更新する
+    });
+  }, []);
+  // ボタンをクリックしたときに画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>他のにゃんこも見る</button>
+      <div>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
+  // ❸ ローディング中でなければ、画像を表示する
+  return <div>{loading || <img src={imageUrl} />}</div>;
+};
+export default IndexPage;
 
-export default IndexPage
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images: unknown = await res.json();
+  // 配列として表現されているか？
+  if (!Array.isArray(images)) {
+    throw new Error("猫の画像が取得できませんでした");
+  }
+  const image: unknown = images[0];
+  // Imageの構造をなしているか？
+  if (!isImage(image)) {
+    throw new Error("猫の画像が取得できませんでした");
+  }
+  return image;
+};
+
+// 型ガード関数
+const isImage = (value: unknown): value is Image => {
+  // 値がオブジェクトなのか？
+  if (!value || typeof value !== "object") {
+    return false;
+  }
+  // urlプロパティが存在し、かつ、それが文字列なのか？
+  return "url" in value && typeof value.url === "string";
+};
